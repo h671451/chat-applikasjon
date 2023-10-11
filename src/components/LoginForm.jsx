279422f786356c7b90eb91b8ccf654abb00e4381
dat20 <huynh123@live.no>
2023-10-11 20:27:09 +0200

    first commit

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,73 @@
+import { useState } from "react"
+import axios from "axios";
+
+
+const LoginForm = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const handleLoginAsGuest = () => {
+        // Set the username and password to the guest credentials
+        setUsername("gjest");
+        setPassword("123456");
+    };
+
+    const handleAdminLogin = () => {
+        // Set the username and password to the guest credentials
+        setUsername("dathuynh");
+        setPassword("123456");
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const authObject = {'Project-ID' : "2505ff91-2fd7-4b95-9530-3951a55e21ad", 'User-Name': username, 'User-Secret': password};
+
+        try {
+            // username | password => chatengine -> gives messages
+            await axios.get('https://api.chatengine.io/chats', {headers: authObject});
+            // works out -> logged inn
+
+            localStorage.setItem('username', username);
+            localStorage.setItem('password', password);
+
+            window.location.reload();
+
+        } catch (error) {
+            // error -> try with new username or password...
+            setError('Incorrect credentials')
+            
+        }
+    }
+
+    return (
+         <div className="wrapper">
+            <div className = "form">
+                <h1 className="title">Chat Application</h1>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="input" placeholder="Username" required />
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="input" placeholder="Password" required />
+                    <div align  = "center">
+                        <button type="submit" className="button">
+                            <span>Start Chatting</span>
+                        </button>
+                    </div>
+                    <h2 className="error">{error}</h2>
+                </form>
+                <div align="center">
+                    <button onClick={handleLoginAsGuest} className="button">
+                        <span>Login as Guest</span>
+                    </button>
+                </div>
+                <div align="center">
+                    <button onClick={handleAdminLogin} className="button">
+                        <span>Login as Dat Huynh(Admin)</span>
+                    </button>
+                </div>
+            </div>
+         </div>
+    );
+}
+
+export default LoginForm;
